5cd465c1b593f7a493b986842fe8f2e9a5d2f9a5
Aliakber Kachwala <ali.kachwala@gmail.com>
2026-06-08 13:10:59 +0100

    fixing user data

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-apt update
+set -e
+
+apt-get update -y
+
+apt-get install -y git curl
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 
-apt install -y nodejs npm git
+apt-get install -y nodejs git
 
 mkdir -p /home/ubuntu/app
 
 cd /home/ubuntu/app
 
 git clone https://github.com/alakbr/api-basics-project.git .
-
-cd app
 
 npm install
 
